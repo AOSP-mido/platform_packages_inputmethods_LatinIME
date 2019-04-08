CyanogenMod/android_packages_inputmethods_LatinIME

LatinIME: Add fallback loader for JNI lib
Prefer the JNI lib with gesture typing support.
If not available / installed fall back to the standard one without
geture typing.

Change-Id: I70a5f43386a9c72809a70bd286f3702378d6f9be

java/src/com/android/inputmethod/latin/define/JniLibName.java
@@ -22,4 +22,5 @@ public final class JniLibName {
     }
 
     public static final String JNI_LIB_NAME = "jni_latinime";
+    public static final String JNI_LIB_NAME2 = "jni_unbundled_latinimegoogle";
 }

AOSP-mido/platform_packages_inputmethods_LatinIME

java-overridable/src/com/android/inputmethod/latin/define/JniLibName.java
@@ -22,4 +22,5 @@ public final class JniLibName {
     }
 
     public static final String JNI_LIB_NAME = "jni_latinime";
+    public static final String JNI_LIB_NAME2 = "jni_latinimegoogle";
 }